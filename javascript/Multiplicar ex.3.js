b75f4f6edc4539162a5f73af1333368cab503b72
talia <taliadacosta_15@hotmail.com>
2025-11-06 22:07:17 -0300

    Finalizado exercicio 3

--- javascript/Multiplicar ex.3.js
+++ javascript/Multiplicar ex.3.js
@@ -5,4 +5,7 @@ const num2 = 6
 
 const resultado = num1*num2
 
-console.log(' Talia comprou uma sacola de mangas no mercados, cada sacola tinha ' +num2+ 'mangas, porém o mercado estava com uma promoção compre uma, ganhe duas' )
+console.log(' Talia comprou uma sacola de mangas no mercados, cada sacola tinha ' +num2+ ' mangas, porém o mercado estava com uma promoção compre uma, ganhe duas.' )
+console.log("Com quantas mangas Talia ficou?")
+console.log('Segue calculo: ' +num1+ ' * ' +num2+ ' = ' )
+console.log(resultado)
